96c7a5a544092751cd9b0a0cbcacc7f8896ab1c2
janaSunrise <warriordefenderz@gmail.com>
2021-04-18 11:58:28 +0530

    Added Base62 Encoder utility.

---
+++ app/utils.py
@@ -0,0 +1,15 @@
+import string
+
+
+def b62_encode(number):
+    base = string.digits + string.lowercase + string.uppercase
+
+    if number == 0:
+        return "0"
+
+    base62 = []
+    while number != 0:
+        number, i = divmod(number, 62)
+        base62.append(base[i])
+
+    return ''.join(reversed(base62))
